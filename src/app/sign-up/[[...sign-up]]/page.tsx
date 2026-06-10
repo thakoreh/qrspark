a186bc4f48ecbcc92e476af07139f4e0e76c4709
Hiren <hiren.thakore58@gmail.com>
2026-06-09 23:43:13 -0400

    Keep auth screens in app

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function SignUpPage() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <SignUp />
+      <SignUp routing="path" path="/sign-up" />
     </div>
   );
 }
